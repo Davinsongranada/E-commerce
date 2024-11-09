adf3fa24c442acda71a8bd9565cabd669a612995
Davinson Granada <118221608+Davinsongranada@users.noreply.github.com>
2024-11-08 19:27:35 -0500

    Cambio de origenes de carpetas

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from main import createTabla
 
 app = FastAPI()
 
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return createTabla()
